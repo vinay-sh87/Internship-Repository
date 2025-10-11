f06bdabc0df4f9e11ecdbb76d0207e424d3180e1
Vinay Sharma <vinaysharma87.dev@gmail.com>
2025-10-12 00:18:18 +0530

    day one and day two codes

---
+++ Javascript/Day1/script.js
@@ -0,0 +1,10 @@
+console.log("message...");
+console.error("Error message");
+console.warn("Warning message");
+console.info("Info message");
+const user = {
+    name: "John",
+    age: 30,
+    city: "New York"
+}
+console.table(user)
---
+++ Javascript/Day2/script.js
@@ -0,0 +1,14 @@
+/* ways to declare a variable 
+var, let and const
+var : can be redeclared , function scoped 
+let : can't be redeclared , block scoped 
+const : value can't be re assigned or redeclared ones initialized , block scoped
+*/
+
+// typeof is a operator that works on a single operand 
+console.log(typeof 5 + 2); // number + 2
+
+let user; // declaration
+user = "Pearson"; // initialization
+console.log(user);
+
